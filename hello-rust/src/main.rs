7af1d1757a0eb0ac8cb7db47aa7574c83db9c008
Josh Long <josh@joshlong.com>
2020-03-10 14:44:26 -0700

    rusty

---
+++ hello-rust/src/main.rs
@@ -0,0 +1,12 @@
+use ferris_says::say;
+use std::io::BufWriter;
+use std::io::{stdout};
+
+fn main() {
+    let stdout = stdout();
+    let out = b"Hello, Mario!";
+    let width = 24;
+    let mut writer = BufWriter::new(stdout.lock());
+    let result = say(out, width, &mut writer);
+    
+}
